5dd1cf14e3f5997e6cd0021e6d8a8216d6a9fca6
Bjorn Mathisen <bcm27@live.com>
2023-04-21 13:52:55 -0500

    Added in powershell scripts for both essential and essential dev tools

---
+++ Download-Essentials.ps1
@@ -0,0 +1,15 @@
+# everything
+# discord
+# spotify
+# steam
+# vlc media player
+# notion enhanced
+# bulk crap uninstaller
+# powershell 7
+# powertoys
+# 7zip
+# sumatra pdf viewer
+# firefox
+# ditto
+
+winget install --id=voidtools.Everything -e  ; winget install --id=Discord.Discord -e  ; winget install --id=Spotify.Spotify -e  ; winget install --id=Valve.Steam -e  ; winget install --id=VideoLAN.VLC -e  ; winget install --id=NotionRepackaged.NotionEnhanced -e  ; winget install --id=Klocman.BulkCrapUninstaller -e  ; winget install --id=Microsoft.PowerShell -e  ; winget install --id=Microsoft.PowerToys -e  ; winget install --id=File-New-Project.EarTrumpet -e  ; winget install --id=7zip.7zip -e  ; winget install --id=Mozilla.Firefox -e  ; winget install --id=SumatraPDF.SumatraPDF -e  ; winget install --id=Ditto.Ditto -e 
